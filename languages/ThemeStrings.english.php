365e39eb600780f6e1f6a1cd62d1daf2e64d7d3a
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-07-12 20:26:02 -0400

    Update ThemeStrings.english.php

--- languages/ThemeStrings.english.php
+++ languages/ThemeStrings.english.php
@@ -1,3 +1,3 @@
 <?php
-$txt['themecopyright'] = 'Ambassador by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> | Updated for SMF 2.1 By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="The Crip Zone Team">TheCripZone</a>';
+$txt['themecopyright'] = 'Ambassador by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> | Updated for SMF 2.1 By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="TheCripZone">TheCripZone</a>';
 ?>
